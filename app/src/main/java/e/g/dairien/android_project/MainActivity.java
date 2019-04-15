1790cff8056f86c1f80bd40c3645fb90b3dfac1d
= <=>
2019-04-15 22:13:36 +0200

    dsd

--- app/src/main/java/e/g/dairien/android_project/MainActivity.java
+++ app/src/main/java/e/g/dairien/android_project/MainActivity.java
@@ -28,17 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar.setTitle("Accueil");
         loadFragment(new AccueilFragment());
-
-
-        Button b =(Button)findViewById(R.id.cata);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent myIntent = new Intent(getApplicationContext(), Catalog.class);
-                startActivity(myIntent);
-            }
-        });
     }
 
     //Handle the bottom bar to load the different fragment
